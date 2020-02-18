49074e0040983d45d418436403854620a4fd5fcf
Alex McGibbon <hello@alexmcgibbon.com>
2020-02-18 08:51:43 +0000

    Adds feeds template

--- logs/views.py
+++ logs/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Feed
 
 def logs_list(request):
     return render(request, 'logs/log_list.html', {})
 
 def feeds_list(request):
-    return render(request, 'logs/feeds/feed_list.html', {})
+	feeds = Feed.objects.order_by('date')
+	return render(request, 'logs/feeds/feed_list.html', {'feeds': feeds})
