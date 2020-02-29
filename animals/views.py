753ec54e91b0cba2729f169af2d5a24ac15c283d
Alex McGibbon <hello@alexmcgibbon.com>
2020-02-29 07:07:02 +0000

    Adds animal templates and static files

--- animals/views.py
+++ animals/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Animal
 
-# Create your views here.
+def animal_list(request):
+	animals = Animal.objects.order_by('name')
+	return render(request, 'animals/animal_index.html', {'animals': animals})
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('logs/', include('logs.urls')),
+    path('', include('animals.urls')),
 ]
